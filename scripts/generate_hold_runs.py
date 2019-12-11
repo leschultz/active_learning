ec9a9ecf4b666929a558edaa3173f68a17feaedf
Lane Enrique Schultz <lsschultz@wisc.edu>
2019-12-11 14:57:45 -0600

    Added the pv runs.

--- scripts/generate_hold_runs.py
+++ scripts/generate_hold_runs.py
@@ -9,37 +9,33 @@ import os
 sys.argv[1:] = [literal_eval(i) for i in sys.argv[1:]]  # Convert types
 
 # Input paramters
-a = sys.argv[1]  # Cubic lattice constant
-elements = sys.argv[2]  # Elements
-numbers = sys.argv[3]  # Number corresponding to each element
-coords = sys.argv[4]  # Starting coordinates
-save = sys.argv[5]  # Location to save POSCAR
-runs = sys.argv[6]  # The number of runs to generate
-incar = sys.argv[7]  # The VASP input file
-kpoints = sys.argv[8]  # The VASP kpoints file
-potcar = sys.argv[9]  # The VASP potential file
-submit = sys.argv[10]  # The cluster submit file
+elements = sys.argv[1]  # Elements
+numbers = sys.argv[2]  # Number corresponding to each element
+coords = sys.argv[3]  # Starting coordinates
+save = sys.argv[4]  # Location to save POSCAR
+runs = sys.argv[5]  # List of cubic lattice constants
+incar = sys.argv[6]  # The VASP input file
+kpoints = sys.argv[7]  # The VASP kpoints file
+potcar = sys.argv[8]  # The VASP potential file
+submit = sys.argv[9]  # The cluster submit file
 
 coords = np.loadtxt(coords)  # Load starting coordinates
 coords = coords/coords.max(axis=0)  # Make fractional
 np.random.shuffle(coords)  # Randomize coordinates
 
-# Generates structure
-structure = functions.gen_cubic(
-                                elements,
-                                numbers,
-                                coords,
-                                a,
-                                )
-
-# Random seeds
-numbers = list(range(runs))
-
 # Generate runs
 count = 1
-total = str(runs)
-for number in numbers:
+total = str(len(runs))
+for number in runs:
     run = os.path.join(save, str(number))
+
+    # Generates structure
+    structure = functions.gen_cubic(
+                                    elements,
+                                    numbers,
+                                    coords,
+                                    number,
+                                    )
 
     # Find matching POTCARS
     pots = []
