68cea5b61c36a7ab32cb9e9029d099a5f119b8de
leschultz <laneenriqueschultz@gmail.com>
2021-06-24 13:13:04 -0500

    Completed loop. Need to test integrity of potential

--- active_learning/run.sh
+++ active_learning/run.sh
@@ -21,14 +21,7 @@ POTS=~/potentials/mlip-2/
 POT=08.mtp
 
 # Clean working space
-#rm -rf runs
-rm -f train.cfg
-rm -f curr.mtp
-rm -f preselected.cfg
-rm -f diff.cfg
-rm -f selected.cfg
-rm -f out.cfg
-
+rm -rf runs
 
 # Directory where run data lives
 mkdir runs
@@ -41,7 +34,6 @@ cd $TEMPER
 mkdir aimd
 cd aimd
 
-: << 'END'
 # Create initial features
 python3 ../../../gen_poscar.py $COMP  # Creates random initial positions
 bash ../../../gen_potcar.sh $COMP $TYPE $RECDIR $RECPOTS  # Creates potential
@@ -61,7 +53,6 @@ Gamma
 touch INCAR
 cat > INCAR <<!
 SYSTEM =  $COMP_$TEMPER
-NCORE = $CORES
 
 # electronic degrees
 LREAL = A                      # real space projection
@@ -75,17 +66,16 @@ ISYM = 0                       # no symmetry
 NELMIN = 4                     # minimum of steps per time step
 NELM = 60                      # maximum of steps per time step
 
-# MD (do little writing to save disc space)
+# MD
 IBRION = 0                     # main molecular dynamics tag
 NSW = 300                      # number of MD steps
 POTIM = 3                      # time step of MD [fs]
-NWRITE = 0                     # controls output
 LCHARG = .FALSE.               # no charge density written out
 LWAVE = .FALSE.                # no wave function coefficients written out
 TEBEG = $TEMPER                # starting temperature for MD
 TEEND = $TEMPER                # end temperature for MD
 
-# canonic (Nose) MD with XDATCAR updated every 10 steps
+# canonic (Nose) MD
 MDALGO = 2                     # switch to select thermostat
 SMASS =  3                     # Nose mass
 ISIF = 2                       # this tag selects the ensemble in combination with the thermostat
@@ -93,7 +83,6 @@ ISIF = 2                       # this tag selects the ensemble in combination wi
 
 # Run AIMD
 $MPI $VASP
-END
 
 MASSES=$(cat POTCAR | grep MASS | awk -F ' ' '{print $3}' | sed 's/\;//g') # Delete
 cd ..
@@ -116,13 +105,15 @@ $MPI mlp train curr.mtp train.cfg --trained-pot-name=curr.mtp
 # Initialize active learning state
 mlp calc-grade curr.mtp train.cfg train.cfg out.cfg --als-filename=state.als
 
+# Copy starting postions to LAMMPS run
+../../../poscar2lammps.awk ../aimd/POSCAR > input.pos
+
 # Start infinite loop
 while [ 1 -gt 0 ]
 do
 
 # Run MD
 touch preselected.cfg
-../../../poscar2lammps.awk ../aimd/POSCAR > input.pos
 
 # Define the masses for classical MD
 COUNTER=1
@@ -152,9 +143,9 @@ neigh_modify    every 1 delay 5 check yes
 
 timestep        0.001
 
-fix             1 all nvt temp ${TEMPER} ${TEMPER} 100.0
+fix             1 all npt temp ${TEMPER} ${TEMPER} 100.0 iso 0.0 0.0 1000.0
 
-thermo_style    custom step temp
+thermo_style    custom step temp press vol pe ke etotal
 thermo 1
 
 dump   1 all custom 1 dump.nb id type x y z fx fy fz
