7b051a5e430154f7d5a64571283c6416b9325231
leschultz <laneenriqueschultz@gmail.com>
2021-07-09 11:34:34 -0500

    Fixed composition bug

--- scripts/run_types/gen_md.sh
+++ scripts/run_types/gen_md.sh
@@ -5,13 +5,17 @@ TOPDIR=$1   # Location of job
 MASSES=$2   # The masses for each element
 ELEMS=$3    # The actual elements
 
+# Convert to arrays
+MASARR=($MASSES)
+ELEARR=($ELEMS)
+
 # Define the masses for classical MD
 COUNTER=1
 rm -f masses.txt
 touch masses.txt
-for i in "${!MASSES[@]}";
+for i in "${!MASARR[@]}";
 do
-    echo "mass $COUNTER ${MASSES[i]}  # ${ELEMS[i]}" >> masses.txt
+    echo "mass $COUNTER ${MASARR[i]}  # ${ELEARR[i]}" >> masses.txt
     COUNTER=$((COUNTER + 1))
 done
 
