96531dca9591ffa2040e3f87f0294c9c58b23dd8
leschultz <laneenriqueschultz@gmail.com>
2021-10-15 14:12:38 -0500

    Added filtering example

--- examples/Si100/active/active_md.sh
+++ examples/Si100/active/active_md.sh
@@ -5,8 +5,9 @@ MPI="prun"                                     # MPI
 LMP="lmp_intel_cpu_intelmpi -in"               # LAMMPS
 VASP='vasp_std'                                # VASP
 WRKDIR=~/packages/active_learning/scripts      # Active Learning Scripts
-PREFIT=../aimd                                 # Initial fit. Every OUTCAR in directory or mlip2 training file.
+PREFIT=../train.cfg                            # Initial fit. Every OUTCAR in directory or mlip2 training file.
 ACTIVE_PARITY=true                             # Whether to produce parity plots for each loop
+FILT_SIGMA=3                                   # Remove energies/atom outside FILT_SIGMA*sigma
 
 # Start active learning
 source $WRKDIR/fit.sh    # Fit the potential
